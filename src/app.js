8141874df55a1563270984bd5ea6cd31a1ac3909
sagargoel072005 <goels4280@gmail.com>
2026-05-05 15:56:09 +0530

    allow all

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ const passport = require("./config/passport");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: true,
     credentials: true,
   })
 );
